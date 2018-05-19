4eb6b6a8883422339083e39a3ee85026b8d30ff5
user <user@WIN-2KR6UHLB0NV>
2018-05-19 10:30:03 +0700

    Degree Converter

--- DegreeConverter.WinView/Program.cs
+++ DegreeConverter.WinView/Program.cs
@@ -8,7 +8,7 @@ namespace DegreeConverter.WinView
 	static class Program
 	{
 		/// <summary>
-		/// The main entry point for the application.
+		/// The main entry point for the application.ccc
 		/// </summary>
 		[STAThread]
 		static void Main()
